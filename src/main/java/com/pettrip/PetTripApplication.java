6b0d3940e9ffb4fc8431d296459474c76a8ae7b2
taehwan <ltw0527@naver.com>
2024-10-30 18:27:47 +0900

    feat : basic configs of spring

---
+++ src/main/java/com/pettrip/PetTripApplication.java
@@ -0,0 +1,11 @@
+package com.pettrip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetTripApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PetTripApplication.class, args);
+    }
+}
