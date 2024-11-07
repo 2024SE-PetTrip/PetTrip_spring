aae59c8a4ac4ccd7d836b3da85e2f5635de7e690
taehwan <ltw0527@naver.com>
2024-11-07 16:09:43 +0900

    feat : 로그인 관련 controller service, repository 추가

--- src/main/java/com/pettrip/domain/User.java
+++ src/main/java/com/pettrip/domain/User.java
@@ -1,4 +1,4 @@
-package togathering.Plogging.domain;
+package com.pettrip.domain;
 
 import lombok.*;
 import com.pettrip.app.dto.JoinDTO;
@@ -19,27 +19,27 @@ public class User extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(unique = true, name = "user_id")
-    private Long id;
+    private Long id;            //auto_increment사용해서 처음 등록한 사람은 1, 그다음 2... 이런식으로 저장
 
     //실명
     @Column(nullable = false, length = 20)
-    private String realname;
+    private String realname;    //실명을 저장하는 entity
 
     @Column(nullable = false, length = 20)
-    private String nickname;
+    private String nickname;    //커뮤니티에서 활동하는 nickname을 추가
 
     //아이디이자 이메일
     @Column(nullable = false, length = 50, unique = true)
-    private String username;
+    private String username;    //로그인시 사용할 아이디이자 이메일
 
     @Column(nullable = false, length = 50)
-    private String password;
+    private String password;    //비밀번호, 저장시 암호화하여 저장
 
     @Column(length = 255)
-    private String profile_image_url;
+    private String profile_image_url;   //이미지는 다른곳에 저장후 링크를 가져오는 방식
 
     @Column(nullable = false, length = 255)
-    private String address;
+    private String address;     //사용자의 주소 저장
 
     //@OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
     //private List<UserPloggingGroupApplyment> //userPloggingGroupApplyments;
