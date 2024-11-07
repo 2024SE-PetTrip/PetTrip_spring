8cd35c32f7d88db3b556830fc4446542ed55e4df
tjgml <tjgml0348@gmail.com>
2024-11-07 11:35:40 +0900

    Feat: add httpStatus

--- src/main/java/com/pettrip/apiPayload/code/status/ErrorStatus.java
+++ src/main/java/com/pettrip/apiPayload/code/status/ErrorStatus.java
@@ -16,21 +16,6 @@ public enum ErrorStatus implements BaseErrorCode {
     _UNAUTHORIZED(HttpStatus.UNAUTHORIZED,401,"인증이 필요합니다."),
     _FORBIDDEN(HttpStatus.FORBIDDEN, 403, "금지된 요청입니다."),
 
-    // 테스트용
-    TEMP_EXCEPTION(HttpStatus.BAD_REQUEST, 4001, "이거는 테스트"),
-
-    // plogging group 생성
-    NOT_FOUND_USER(HttpStatus.BAD_REQUEST, 40401, "존재하지 않는 유저입니다."),
-    NOT_FOUND_GROUP(HttpStatus.BAD_REQUEST, 40402, "존재하지 않는 그룹입니다."),
-    NOT_FOUND_COURSE(HttpStatus.BAD_REQUEST, 40403, "존재하지 않는 코스입니다."),
-
-    ALREADY_JOIN_GROUP(HttpStatus.BAD_REQUEST, 40901, "이미 참가 중인 그룹입니다."),
-    ALREADY_EXIT_GROUP(HttpStatus.BAD_REQUEST, 40902, "이미 탈퇴한 그룹입니다."),
-    ADMIN_CANNOT_EXIT(HttpStatus.BAD_REQUEST, 40903, "그룹장은 탈퇴할 수 없습니다."),
-
-    INVALID_STATUS_CHANGE(HttpStatus.BAD_REQUEST, 40601, "플로깅 시작에 실패하였습니다."),
-    NOT_GROUP_ADMIN(HttpStatus.BAD_REQUEST, 40602, "그룹장이 아닙니다."),
-    NOT_FOUND_APPLYMENT(HttpStatus.BAD_REQUEST, 40603, "해당 그룹에 참여하지 않은 유저입니다."),
 
     // 회원가입 실패
     REGISTER_ALREADY_USER_EXIST(HttpStatus.BAD_REQUEST, 41001, "이미 있는 아이디(이메일)입니다."),
@@ -42,7 +27,14 @@ public enum ErrorStatus implements BaseErrorCode {
     REGISTER_EMAIL_TOO_LONG(HttpStatus.BAD_REQUEST, 41006, "이메일은 최대 50자입니다."),
     REGISTER_PASSWORD_INVALID(HttpStatus.BAD_REQUEST, 41007, "비밀번호 형식이 아닙니다."),
     REGISTER_PASSWORD_EMPTY(HttpStatus.BAD_REQUEST, 41008, "비밀번호를 입력해주세요."),
-    REGISTER_PASSWORD_TOO_LONG(HttpStatus.BAD_REQUEST, 41009, "비밀번호는 최대 70자입니다.");
+    REGISTER_PASSWORD_TOO_LONG(HttpStatus.BAD_REQUEST, 41009, "비밀번호는 최대 70자입니다."),
+
+    NOT_FOUND_USER(HttpStatus.BAD_REQUEST, 40401, "존재하지 않는 회원입니다."),
+    NOT_FOUND_CARE_REQUEST(HttpStatus.BAD_REQUEST, 40404, "존재하지 않는 돌봄요청입니다."),
+    INVALID_MATCH_STATUS(HttpStatus.BAD_REQUEST, 40405, "돌봄 서비스를 매칭할 수 없습니다."),
+    INVALID_EVALUATION_STATUS(HttpStatus.BAD_REQUEST, 40406, "돌봄 서비스를 평가할 수 없습니다.");
+
+
 
     // 여기 관련해서 하고싶은 응답들 정의해주세요..
 
--- src/main/java/com/pettrip/apiPayload/code/status/SuccessStatus.java
+++ src/main/java/com/pettrip/apiPayload/code/status/SuccessStatus.java
@@ -36,6 +36,12 @@ public enum SuccessStatus implements BaseCode {
     //돌봄
     CARE_REQUEST_OK(HttpStatus.OK, 200, "돌봄 요청 생성 성공"),
     CARE_REQUEST_LIST_OK(HttpStatus.OK, 200, "돌봄 요청 리스트 조회 성공"),
+    CARE_REQUEST_DETAIL_OK(HttpStatus.OK, 200, "돌봄 요청 상세 조회 성공"),
+    CARE_REQUEST_UPDATED(HttpStatus.OK, 200, "돌봄 요청 업데이트 성공"),
+    CARE_REQUEST_DELETED(HttpStatus.OK, 200, "돌봄 요청 삭제 성공"),
+    CARE_PROVIDER_MATCHED(HttpStatus.OK, 200, "돌봄 매칭 성공"),
+    CARE_EVALUATION_OK(HttpStatus.OK, 200, "돌봄 평가 완료"),
+    CARE_EVALUATION_LIST_OK(HttpStatus.OK, 200, "돌봄 제공자의 평가 내역 조회 성공"),
 
     // 회원가입 관련
     REGISTER_JOIN_OK(HttpStatus.OK, 200, "회원가입 성공!");
